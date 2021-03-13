4df4b797aed8ed4850bc5bb590ea783cd99f5277
justplainhungry <78654953+justplainhungry@users.noreply.github.com>
2021-03-13 13:06:58 -0500

    project 2; beginning
    Added documentation comments; corrected the errors, adapted to project 1 solution

--- Policy.java
+++ Policy.java
@@ -1,12 +1,11 @@
 /**
    Project part 1. 
-      COMPLETE LAST -> NOTES FOR METHODS.
    This Policy class models an insurance policy for a single person.
 */
 
 public class Policy
 {  
-   // Set up class fields.
+   // Set up class fields. check
    private String policyNumber;
    private String providerName;
    private String holderFirstName;
@@ -15,203 +14,219 @@ public class Policy
    private double age;
    private double heightInch;
    private double weightLbs;
-   private double BMI;
-   private double price;
-   private boolean smokerBool; 
-   private double BASE_FEE;                      
-   private double AGE_FEE;                       
-   private double SMOKER_FEE;                     
-   private int AGE_LIMIT;                           
-   private double BMI_SUB;                           
-   private double BMI_MULTI;
-   private double WEIGHT_MULTIPLIER;                      
+            
 
+   // Set up no-arg constructor. check
+   public Policy()
+   {
+      policyNumber = "";
+      providerName = "";
+      holderFirstName = "";
+      holderLastName = "";
+      smokerStr = "";
+      age = 0.0;
+      heightInch = 0.0;
+      weightLbs = 0.0;                     
+
+   }
    
-   // Set up constructor.
-   public Policy(String pn, String provN, String hfn, String hln, double a, double hi, double wl)
+   // Set up constructor. check
+   public Policy(String pn, String provN, String hfn, String hln, String smk, double a, double hi, double wl)
    {
       policyNumber = pn;
       providerName = provN;
       holderFirstName = hfn;
       holderLastName = hln;
+      smokerStr = smk;
       age = a;
       heightInch = hi;
       weightLbs = wl;
    }
-   
-   // Set up no-arg constructor.
-   public Policy()
-   {
-      final double BASE_FEE = 600.0;                      
-      final double AGE_FEE = 75.0;                        
-      final double SMOKER_FEE = 100.0;                     
-      final int AGE_LIMIT = 50;                           
-      final double BMI_SUB = 35;                           
-      final double BMI_MULTI = 20; 
-      final double WEIGHT_MULTIPLIER = 703.0;                       
-
-   }
-   
+      
    // Create setters.
+   
+   /**
+      This method sets the holder's policy number.
+   */
    public void setPolicyNumber(String pn)
    {
       policyNumber = pn;
    }
    
+   /**
+      This method sets the holder's provider name.
+   */
    public void setProviderName(String provN)
    {
       providerName = provN;
    }
    
+   /**
+      This method sets the policy holder's first name.
+   */
    public void setHolderFirstName(String hfn)
    {
       holderFirstName = hfn;
    }
    
+   /**
+      This method sets the policy holder's last name.
+   */
    public void setHolderLastName(String hln)
    {
       holderLastName = hln;
    }
    
+   /**
+      Thie method sets the policy holder's age.   
+   */
    public void setAge(double a)
    {
       age = a;
    }
    
+   /**
+      This method sets the policy holder's smoking status.
+   */
    public void setSmokeStr(String sm)
    {
       smokerStr = sm;
    }
    
+   /**
+      This method sets the policy holder's height.
+   */
    public void setHeightInch(double hi)
    {
       heightInch = hi;
    }
    
+   /**
+      This method sets the policy holder's weight.
+   */
    public void setWeightLbs(double wl)
    {
       weightLbs = wl;
    }
    
    // Create getters.
+   
+   /**
+      This method gets the policy holder's policy number.
+      @return policyNumber Holds the policy number.
+   */
    public String getPolicyNumber()
    {
       return policyNumber;
    }
    
+   /**
+      This method gets the insurance provider's name
+      @return providerName Holds the provider's name.
+   */
    public String getProviderName()
    {
       return providerName;
    }
    
+   /**
+      This method gets the policy holder's first name.
+      @return holderFirstName Holds the holder's first name.
+   */
    public String getHolderFirstName()
    {
       return holderFirstName;
    }
    
+   /**
+      This method gets the policy holder's last name.
+      @return holderLastName Policy holder's lastName.
+   */
    public String getHolderLastName()
    {
       return holderLastName;
    }
    
+   /**
+      This method gets the policy holder's age.
+      @return age Policy holder's age.
+   */
    public double getAge()
    {
       return age;
    }
    
+   /**
+      This method returns the smoking status as a string.
+      @return smokerStr Policy holder's smoking status.
+   */
    public String getSmokeStr()
    {
       return smokerStr;
    }
    
+   /**
+      This method returns the policy holder's height in inches.
+      @return heightInch Policy holder's height in inches.
+   */
    public double getHeightInch()
    {
       return heightInch;
    }
    
+   /**
+      This method gets the policy holder's weight in pounds.
+      @return weightLbs Policy holder's weight in pounds.
+   */
    public double getWeightLbs()
    {
       return weightLbs;
    }
    
+   
+   
    /**
-      This method 
+      This method calculates and returns the policy holder's BMI.
+      @return BMI Policy holder's BMI.
    */
-   public boolean convertBooleanSmoker()
-   {
-      if (smokerStr == "smoker")
-      {
-         smokerBool = true;
-      }
-      else if (smokerStr == "non-smoker")
-      {
-         smokerBool = false;
-      }
-      else 
-      {
-         System.out.print("ERROR: You must enter either smoker or non-smoker.");
-         System.out.println("Please restart the program.");
-         System.exit(0);
-      }
-      
-      return smokerBool;
-   }
-
-   // Calculate and return the policy holder's BMI and return it.
    public double calculateHolderBMI()  
    {
-      final double WEIGHT_MULTIPLIER = 703.0;
-      double BMI;
-      double insertBMI;
+      final double WEIGHT_MULTIPLIER = 703.0; 
       
-      BMI = (weightLbs * WEIGHT_MULTIPLIER)/(heightInch * heightInch); 
-      insertBMI = calculatePolicyPrice(BMI);
-      return BMI;
+      return (weightLbs * WEIGHT_MULTIPLIER)/(heightInch * heightInch); 
    }
    
-   // Calculate the holder's policy price and return it.
-   public static double calculatePolicyPrice(double BMI) 
+   /**
+      This method calculates the holder's policy price and returns it.
+      @return price the policy's price for the holder.
+   */
+   public double calculatePolicyPrice() 
    {  
-      final double BASE_FEE = 600.0;                      
-      final double AGE_FEE = 75.0;                        
-      final double SMOKER_FEE = 100.0;                     
+      final double BASE_FEE = 600.0;                    
+      final double AGE_FEE = 75.0;                    
+      final double SMOKER_FEE = 100.0;
+      final double BMI_MULTI = 20;     
+      
       final int AGE_LIMIT = 50;                           
-      final double BMI_SUB = 35;                           
-      final double BMI_MULTI = 20; 
-      double price; 
-      double BMIFee;
-      double zeroFee = 0.0;
-                                                                      
-      price = BASE_FEE;
+      final int BMI_THRESHOLD = 35;                           
       
-      if (age >= AGE_LIMIT)
+      double price = BASE_FEE;                                  
+      
+      if (age >= AGE_LIMIT) 
       {
-         price += AGE_FEE;
-      }
-      else
+         price += AGE_FEE; 
+      }                         
+                               
+      if (smokerStr.equalsIgnoreCase("smoker"));
       {
-         price += zeroFee;
+         price += SMOKER_FEE;  // for some reason my code keeps adding $100 smoking fee, even when non-smoker is chosen. 
       }
       
-      if (smokerBool == true)
+      if (calculateHolderBMI() > BMI_THRESHOLD) 
       {
-         price += SMOKER_FEE;
+         price += (calculateHolderBMI() - BMI_THRESHOLD) * BMI_MULTI;
       }
-      else
-      {
-         price += zeroFee;         
-      }
-      
-      if (BMI >= 35.0)// OUTPUT DOESN'T CALCULATE PRICE WITH BMI ??
-      {
-         BMIFee = (BMI - BMI_SUB) * BMI_MULTI;
-         price += BMIFee;
-      }
-      else
-      {
-         price += zeroFee;
-      }
-      
+           
       return price;   
    } 
 }
