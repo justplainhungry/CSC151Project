827cb9bb362ab1f207d545a16872d75712ec805c
justplainhungry <78654953+justplainhungry@users.noreply.github.com>
2021-03-13 22:09:26 -0500

    Made to hold data from PolicyInformation.txt

--- Demo.java
+++ Demo.java
@@ -1,9 +1,10 @@
-// Main method for CSC151 project part 1.
+// Main method for CSC151 project part 2.
 import java.util.Scanner;
+import java.io.*;
 
 public class Demo
 {
-   public static void main(String[] args) 
+   public static void main(String[] args) throws IOException
    {
       // innitialize variables.
       String policyNumber = " ";
@@ -15,61 +16,68 @@ public class Demo
       double heightInch = 0.0;
       double weightLbs = 0.0;
       
-      // Set up the keyboard for input.
-      Scanner keyboard = new Scanner(System.in);
-      
+      // Open the file.
+      File file = new File("PolicyInformation.txt");
+      Scanner inputFile = new Scanner(file);
+            
       // Create object for the Policy class.
       Policy newHolder = new Policy();
       
-      // Prompt user for input and store.
-      System.out.print("Please enter the Policy Number: ");
-      policyNumber = keyboard.nextLine();
-      newHolder.setPolicyNumber(policyNumber);
       
-      System.out.print("Please enter the Provider Name: ");
-      providerName = keyboard.nextLine();
-      newHolder.setProviderName(providerName);
+      // Read from the PolicyInformation.txt file
       
-      System.out.print("Please enter the Policyholder's First Name: ");
-      holderFirstName = keyboard.nextLine();
-      newHolder.setHolderFirstName(holderFirstName);
+      while (inputFile.hasNext())
+      {
+         policyNumber = inputFile.nextLine();
+         newHolder.setPolicyNumber(policyNumber);
+          
+         providerName = inputFile.nextLine();
+         newHolder.setProviderName(providerName);
+         
+         holderFirstName = inputFile.nextLine();
+         newHolder.setHolderFirstName(holderFirstName);
+         
+         holderLastName = inputFile.nextLine();
+         newHolder.setHolderLastName(holderLastName);
+         
+         holderAge = inputFile.nextInt();
+         newHolder.setAge(holderAge);
+         
+         smoker = inputFile.nextLine();
+         newHolder.setSmokeStr(smoker);
+         
+         inputFile.nextLine();
       
-      System.out.print("Please enter the Policyholder's Last Name: ");
-      holderLastName = keyboard.nextLine();
-      newHolder.setHolderLastName(holderLastName);
+         heightInch = inputFile.nextDouble();
+         newHolder.setHeightInch(heightInch);
       
-      System.out.print("Please enter the Policyholder's Age: ");
-      holderAge = keyboard.nextInt();
-      newHolder.setAge(holderAge);
+         weightLbs = inputFile.nextDouble();
+         newHolder.setWeightLbs(weightLbs);
+         
+         // Output data.
+         System.out.println("\n\nPolicy Number: " + newHolder.getPolicyNumber());
+         System.out.println("Provider Name: " + newHolder.getProviderName());
+         System.out.println("Policyholder's First Name: " + newHolder.getHolderFirstName());
+         System.out.println("Policyholder's Last Name: " + newHolder.getHolderLastName());
+         System.out.println("Policyholder's Age: " + newHolder.getAge());
+         System.out.println("Policyholder's Smoking Status: " + newHolder.getSmokeStr());
+         System.out.println("Policyholder's Height: " + newHolder.getHeightInch());
+         System.out.println("Policyholder's Weight: " + newHolder.getWeightLbs());
+         System.out.printf("Policyholder's BMI: %,.2f", newHolder.calculateHolderBMI());
+         System.out.printf("\nPolicy Price: $%,.2f", newHolder.calculatePolicyPrice());
+      
+         
+         inputFile.nextLine();
+         if(inputFile.hasNext())
+         {
+             inputFile.nextLine();//this handles the end of the file to avoid an exception}
+         }
+
+      }
       
       // Clear the keyboard Buffer.
-      keyboard.nextLine();
+           
       
-      System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-      smoker = keyboard.nextLine();
-      newHolder.setSmokeStr(smoker);
-      
-      System.out.print("Please enter the Policyholder's Height (in inches): ");
-      heightInch = keyboard.nextDouble();
-      newHolder.setHeightInch(heightInch);
-      
-      System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-      weightLbs = keyboard.nextDouble();
-      newHolder.setWeightLbs(weightLbs);
-      
-      
-      // Output data.
-      System.out.println("\n\nPolicy Number: " + newHolder.getPolicyNumber());
-      System.out.println("Provider Name: " + newHolder.getProviderName());
-      System.out.println("Policyholder's First Name: " + newHolder.getHolderFirstName());
-      System.out.println("Policyholder's Last Name: " + newHolder.getHolderLastName());
-      System.out.println("Policyholder's Age: " + newHolder.getAge());
-      System.out.println("Policyholder's Smoking Status: " + newHolder.getSmokeStr());
-      System.out.println("Policyholder's Height: " + newHolder.getHeightInch());
-      System.out.println("Policyholder's Weight: " + newHolder.getWeightLbs());
-      System.out.printf("Policyholder's BMI: %,.2f", newHolder.calculateHolderBMI());
-      System.out.printf("\nPolicy Price: $%,.2f", newHolder.calculatePolicyPrice());
-      
-      
+            
    }
 }
